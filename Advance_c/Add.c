8ab2f972b1746173bc144ddcb1a78f1155333b6a
belaletech <belalahmadswnbhr@gmail.com>
2022-12-12 04:34:40 -0800

    sum of two number

---
+++ Advance_c/Add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter your 1st number\n");
+    scanf("%d",&a);
+    printf("Enter the second number \n");
+    scanf("%d",&b);
+    c=a+b;
+    printf("sum= %d",c);
+
+}
