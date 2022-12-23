74f58475c8a518c699a4deeabb0f286595f08be6
Belal ahmad <92047716+belaletech@users.noreply.github.com>
2022-12-23 21:37:54 +0000

    Division of two number

---
+++ Advance_c/Pointer/Division.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    int *p,*q,*r;
+    p=&a;
+    q=&b;
+    r=&c;
+    printf("Enter Two number \n");
+    scanf("%d%d",p,q);
+    *r=*p/ *q;
+    printf("Division=%d ",*r);
+}
