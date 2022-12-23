f76fea20efdb49c9469b3de9606138f176b4cb29
Belal ahmad <92047716+belaletech@users.noreply.github.com>
2022-12-23 21:39:30 +0000

    update

--- Advance_c/Pointer/Division.c
+++ Advance_c/Pointer/Division.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    int *p,*q,*r;
+    float a,b,c;
+    float *p,*q,*r;
     p=&a;
     q=&b;
     r=&c;
     printf("Enter Two number \n");
-    scanf("%d%d",p,q);
+    scanf("%f%f",p,q);
     *r=*p/ *q;
-    printf("Division=%d ",*r);
+    printf("Division=%f ",*r);
 }
