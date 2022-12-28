bb6c6b4bd2b929d25a0a5f32d9ab3877c54558c4
belaletech <belalahmadswnbhr@gmail.com>
2022-12-28 16:55:23 +0530

    Print all even numbers of single dimentinla array

---
+++ Advance_c/ARRAY/Even_1D_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],n;
+    printf("Enter how many number you want to store\n");
+    scanf("%d",&n);
+    printf("Enter %d number one by one\n",n);
+    for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    printf("Array element is given below\n");
+    for(int j=0;j<n;j++)
+    printf("%d ",a[j]);
+    printf("\n Even number is given below\n");
+    for(int k=0;k<n;k++)
+    {
+        if(a[k]%2==0)
+        printf("%d ",a[k]);
+
+    }
+}
