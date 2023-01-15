306a33544a5dbe60d05cc49dc1a57d97c87dbe31
belaletech <belalahmadswnbhr@gmail.com>
2023-01-15 15:06:16 +0530

    update

--- Advance_c/2D_ARRAY/Print_Element.c
+++ Advance_c/2D_ARRAY/Print_Element.c
@@ -4,4 +4,5 @@ int main()
     int a[3][3]={{2,1,9},{5,6,7}};
     printf("Value at a[0][]=%d\n",a[0][0]);
     printf("value at a[0][1]=%d\n",a[0][1]);
+    printf("value at a[1][0]=%d",a[1][0]);
 }
