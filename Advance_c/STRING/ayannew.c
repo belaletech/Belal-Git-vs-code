ee77376f4645b205fc20843d6cef1cc58f3468cc
belaletech <belalahmadswnbhr@gmail.com>
2023-01-20 17:52:28 +0530

    update

---
+++ Advance_c/STRING/ayannew.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char str[30];
+    printf("Enter your name\n");
+    //gets(str);
+    scanf("%s",str);
+    while (str!=0)
+    {
+        
+    printf("%s",str); 
+    }
+    
+   
+}
